2bd6e34b8d9af70a45b4819d0bda91f0513d61d1
wooziekim <59155317+wooziekim@users.noreply.github.com>
2020-06-04 17:52:26 +0900

    Initial commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# resume
-Created with CodeSandbox
---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>김태우(Taewoo Kim)</h1>
+      <h2 styles="text-align:left">
+        ■ Age : 만31
+        <br />
+        ■ Trello : <br />■ wooziekim@gmail.com
+      </h2>
+    </div>
+  );
+}
